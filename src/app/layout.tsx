701cd258c6dcaa497e4439626a994fe37dc2e2b4
SoraKumo <info@croud.jp>
2026-01-28 17:13:22 +0900

    Simplify protocol detection using 'via' header existence.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -21,7 +21,7 @@ const geistMono = Geist_Mono({
 async function getOrigin() {
   const headersList = await headers();
   const host = headersList.get("x-forwarded-host") || headersList.get("host");
-  const protocol = headersList.get("via")?.includes("HTTP/2")
+  const protocol = headersList.get("via")
     ? "https"
     : (headersList.get("x-forwarded-proto") ?? "http");
   return `${protocol}://${host}`;
